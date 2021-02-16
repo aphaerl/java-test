7c01c248411a8b5f99c4490e6ddfdbbc9e275c8a
Trutix-dev <ptruta@icloud.com>
2021-02-16 15:27:02 +0100

    Lmao what a meme

--- GitTest/src/com/oda/test/HelloWorld.java
+++ GitTest/src/com/oda/test/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt!");
 		System.out.println("New line.");
+		System.out.println("Lmao get rekt git")
+		//Git deez nuts xD
 	}
 
 }
