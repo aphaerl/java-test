1a3c97051daf48b3cf37c5fc464f9c65b44e2a3c
Raphael <Raphael_Test@gmx.de>
2021-02-16 15:29:39 +0100

    Mistakes were made.

--- GitTest/src/com/oda/test/HelloWorld.java
+++ GitTest/src/com/oda/test/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt!");
 		System.out.println("New line.");
-		System.out.println("Lmao get rekt git")
+		System.out.println("Lmao get rekt git");
 		//Git deez nuts xD
 	}
 
